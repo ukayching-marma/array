794ff165727d53f7c3990449315127245ec38489
ukayching-marma <ukaychingmarma100@gmail.com>
2023-07-31 15:19:36 +0600

    My first array

---
+++ array.js
@@ -0,0 +1,13 @@
+// Array
+var friendsAge = [11, 21, 45, 17, 14, 105, 6];
+var picnicfee = [5000, 2000, 4000, 150];
+var nayikas = ['mahi', 'opu', 'priyanka'];
+var vowels = ['a', 'e', 'i', 'o', 'u']
+console.log(friendsAge);
+console.log(picnicfee);
+console.log(nayikas);
+console.log(vowels);
+console.log(friendsAge.length);
+console.log(picnicfee.length);
+console.log(nayikas.length);
+console.log(vowels.length);
